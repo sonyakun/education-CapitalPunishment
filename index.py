eb52aae35a28e924f54d662d53ea5495bdc5ead6
sonyakunYT <90973770+sonyakunYT@users.noreply.github.com>
2023-08-14 18:52:32 +0900

    教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育死刑死刑死刑死刑死刑死刑死刑死刑死刑教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育教育

---
+++ index.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/gen/{text1}/")
+def read_root(edu: str="教育", cp: str="死刑"):
+  n1 = []
+  n2 = []
+  n3 = []
+  for i in range(18):
+      n1.append(edu)
+  for i in range(10):
+      n2.append(cp)
+  for i in range(19):
+      n3.append(edu)
+  return {"result": "".join(n1) + "".join(n2) + "".join(n3)}
+
+uvicorn.run(app)
